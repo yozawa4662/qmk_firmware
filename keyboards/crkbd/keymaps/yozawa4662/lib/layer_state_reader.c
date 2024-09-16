e60a8aab2cdda891a491f0ed75e27a53f5b84c4a
yozawa4662 <yuyaron01@gmail.com>
2024-09-16 23:08:00 +0900

    :sparkles: Change layer reader prefix

--- keyboards/crkbd/keymaps/yozawa4662/lib/layer_state_reader.c
+++ keyboards/crkbd/keymaps/yozawa4662/lib/layer_state_reader.c
@@ -14,23 +14,19 @@ const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "LAYER: efault");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
     break;
   case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "LAYER: Raise");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
     break;
   case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "LAYER: Lower");
-    break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "LAYER: Adjust");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
     break;
   default:
 #if defined (LAYER_STATE_32BIT)
-    snprintf(layer_state_str, sizeof(layer_state_str), "LAYER: Undef-%ld", layer_state);
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
 #else
-    snprintf(layer_state_str, sizeof(layer_state_str), "LAYER: Undef-%d", layer_state);
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%d", layer_state);
 #endif
   }
 
