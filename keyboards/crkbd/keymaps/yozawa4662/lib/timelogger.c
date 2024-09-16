66f3bce978b0eaab00758fe7bcb3cd0346fac449
yozawa4662 <yuyaron01@gmail.com>
2024-09-16 23:07:31 +0900

    :sparkles: Change timelogger prefix

--- keyboards/crkbd/keymaps/yozawa4662/lib/timelogger.c
+++ keyboards/crkbd/keymaps/yozawa4662/lib/timelogger.c
@@ -8,7 +8,7 @@ int elapsed_time = 0;
 void set_timelog(void) {
   elapsed_time = timer_elapsed(last_time);
   last_time = timer_read();
-  snprintf(timelog_str, sizeof(timelog_str), "TTERM:%d", elapsed_time);
+  snprintf(timelog_str, sizeof(timelog_str), "TTerm: %dms", elapsed_time);
 }
 
 const char *read_timelog(void) {
